8a122561455a49e04e52b748c4b44ca3a4800382
AdairAvila <adairavilaromerocomic@gmail.com>
2024-07-16 12:31:44 -0600

    asignacion, console.log y tipeof

---
+++ saludo.js
@@ -0,0 +1,11 @@
+var nombre = "Adair";
+var edad= "26";
+var presenteOno = true;
+var apellido = "Avila";
+var estatura = 1.70;
+
+console.log("Nombre: " + nombre + "" + typeof nombre);
+console.log("Edad: " + edad + "" + typeof edad);
+console.log("Presente: " + presenteOno + "" + typeof presenteOno);
+console.log("Apellido: " + apellido + "" + typeof apellido);
+console.log("Estatura: " + estatura + "" + typeof estatura);
